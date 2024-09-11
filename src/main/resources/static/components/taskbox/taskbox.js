d06fc9d6418a497a7d6f4fd000366838f6602a3b
Thomas Anker Jensen <thakje8@gmail.com>
2024-09-11 14:37:51 +0200

    oppdaterte filer

--- src/main/resources/static/components/taskbox/taskbox.js
+++ src/main/resources/static/components/taskbox/taskbox.js
@@ -1,3 +1,28 @@
-/**
- * 
- */
+class Taskbox extends HTMLElement {
+    
+    constructor() {
+    super();
+      
+    const template = document.createElement('template');
+    template.innerHTML = `
+    
+        <div class="task-box> 
+            <h3 class="Wash floor> </h3>
+            <p class="Done"> </p>
+        </div>  
+      `;
+      
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      shadowRoot.appendChild(template.content.cloneNode(true));
+      
+        }
+        
+        connectedCallBack() {
+            
+            this.shadowRoot.querySelector('.Wash floor').textContent = this.getAttribute('task') || "uknown task";
+            this.shadowRoot.querySelector('.Done').textContent = this.getAttribute('status') || "uknown status";
+                
+        }
+    }
+    
+    customElements.define('task-box', Taskbox);
--- src/main/resources/static/components/taskview/taskview.js
+++ src/main/resources/static/components/taskview/taskview.js
@@ -1,38 +1,32 @@
 import '../tasklist/tasklist.js';
 import '../taskbox/taskbox.js';
-
-const template = document.createElement("template");
-template.innerHTML = `
- 
-<link rel="stylesheet" type="text/css"href="${import.meta.url.match(/.*\//)[0]}/taskview.css"/>
-
- <h1>Tasks</h1>
- 
- <div id="message"> <p>Waiting for server data.</p> </div>
- 
- <div id="newtask"> <button type="button" disabled>New task</button> </div>
- 
- <!-- The task list -->
- <task-list></task-list>
-
- <!-- The Modal -->
- <task-box></task-box>
- `;
- 
  class TaskView extends HTMLElement {
-  
-    
     constructor() {
     super(); 
+
+    const template = document.createElement("template");
+    template.innerHTML = `
+ 
+        <link rel="stylesheet" type="text/css"href="${import.meta.url.match(/.*\//)[0]}/taskview.css"/>
+
+        <h1>Tasks</h1>
+ 
+        <div id="message"> <p>Waiting for server data.</p> </div>
+ 
+        <div id="newtask"> <button type="button" disabled>New task</button> </div>
+ 
+        <!-- The task list -->
+        <task-list> </task-list>
+
+        <!-- The Modal -->
+        <task-box></task-box>
+ `;
+ 
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(template.content.cloneNode(true));
     
     }
-    
-
-
-
-
 }
     
- 
  customElements.define('task-view', TaskView);
  
